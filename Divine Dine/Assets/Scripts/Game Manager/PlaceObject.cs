5e4e989f2bd2ce5bdd775b65ed2d756ba52476b4
ImpDesign <impdesign.bm@gmail.com>
2016-02-15 12:27:28 -0700

    Rotate Object during placement
    You can now hold down left click after placing the object and move your
    mouse around to rotate the object.

--- Divine Dine/Assets/Scripts/Game Manager/PlaceObject.cs
+++ Divine Dine/Assets/Scripts/Game Manager/PlaceObject.cs
@@ -8,13 +8,17 @@ public class PlaceObject : MonoBehaviour {
     private bool active = false;                           //If you are able to place an object with the mouse
     private bool canRotate = false;                        //Option to rotate object if just placed in scene 
     private int selectedPrefab;                            //Which object in the list you are going to place
-    private float prefabOffset;                            //The Y offset position from the tile you are placing the new object on
-    private Object previewObject;                          //Object created in the scene that is just a preview
+    private float prefabSpaceOffset;                       //The Y offset position from the tile you are placing the new object on
+    private float prefabRotationOffset;                    //The rotation offset so it faces south
+    private GameObject previewObject;                      //Object created in the scene that is just a preview
+    private GameObject placedObject;                       //Object created in the scene after clicking
     private Vector3 previewFloor = Vector3.zero;           //Floorspace that the preview is ontop of
+    private Vector3 placedFloor = Vector3.zero;            //Floorspace that the placed object is ontop of
+    private Vector3 facingFloor = Vector3.zero;            //Floorspace that the placed object should face during rotation
 
 	void Update ()
     {
-        if (active)
+        if (active && !canRotate)
         {
             ShowPreview();
             //Place the new object by left clicking
@@ -23,13 +27,18 @@ public class PlaceObject : MonoBehaviour {
                 CreateObject();
             }
         }
+        else if(canRotate)
+        {
+            RotateObject();
+        }
     }
 
     //Select a prefab from the list
     public void SetPrefab(int prefabNumber)
     {
         selectedPrefab = prefabNumber;
-        prefabOffset = placeablePrefabs[selectedPrefab].GetComponent<PlaceableObject>().offset;
+        prefabSpaceOffset = placeablePrefabs[selectedPrefab].transform.position.y;
+        prefabRotationOffset = placeablePrefabs[selectedPrefab].GetComponent<PlaceableObject>().rotationOffset;
     }
 
     //Allow or disallow the placeing of objects
@@ -46,14 +55,22 @@ public class PlaceObject : MonoBehaviour {
         {
             if (hit.transform.tag == "Floor" && !hit.transform.GetComponent<FloorBehavior>().IsUsed())
             {
+                //Destroy preview
+                Destroy(previewObject);
+                previewFloor = Vector3.zero;
+                placedFloor = hit.transform.position;
+
                 //Create object in scene
-                Instantiate(placeablePrefabs[selectedPrefab],
-                    new Vector3(hit.transform.position.x, hit.transform.position.y + prefabOffset, hit.transform.position.z),
-                    hit.transform.rotation);
+                placedObject = Instantiate(placeablePrefabs[selectedPrefab],
+                    new Vector3(hit.transform.position.x, hit.transform.position.y + prefabSpaceOffset, hit.transform.position.z),
+                    placeablePrefabs[selectedPrefab].transform.rotation) as GameObject;
 
                 //Update floor behavior
                 hit.transform.GetComponent<FloorBehavior>().SetUsed(true);
                 hit.transform.GetComponent<FloorBehavior>().SetObjTag(placeablePrefabs[selectedPrefab].tag);
+
+                //Allow the player to rotate the object
+                canRotate = true;
 
                 //Keep creating objects if they hold control
                 if (!Input.GetKey(KeyCode.LeftControl))
@@ -82,11 +99,42 @@ public class PlaceObject : MonoBehaviour {
 
                 //Create object in scene
                 previewObject = Instantiate(placeablePrefabs[selectedPrefab],
-                    new Vector3(hit.transform.position.x, hit.transform.position.y + prefabOffset, hit.transform.position.z),
-                    hit.transform.rotation);
+                    new Vector3(hit.transform.position.x, hit.transform.position.y + prefabSpaceOffset, hit.transform.position.z),
+                    placeablePrefabs[selectedPrefab].transform.rotation) as GameObject;
 
                 previewFloor = hit.transform.position;
             }
+        }
+    }
+
+    private void RotateObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            facingFloor = hit.transform.position;
+
+            if (facingFloor.x - placedFloor.x < 0)
+            {
+                placedObject.transform.eulerAngles = (new Vector3(0, prefabRotationOffset+ 90, 0));
+            }
+            else if (facingFloor.x - placedFloor.x > 0)
+            {
+                placedObject.transform.eulerAngles = (new Vector3(0, prefabRotationOffset + 270, 0));
+            }
+            else if (facingFloor.z - placedFloor.z < 0)
+            {
+                placedObject.transform.eulerAngles = (new Vector3(0, prefabRotationOffset, 0));
+            }
+            else if (facingFloor.z - placedFloor.z > 0)
+            {
+                placedObject.transform.eulerAngles = (new Vector3(0, prefabRotationOffset + 180, 0));
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            canRotate = false;
         }
     }
 }
--- Divine Dine/Assets/Scripts/Objects/PlaceableObject.cs
+++ Divine Dine/Assets/Scripts/Objects/PlaceableObject.cs
@@ -1,19 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlaceableObject : MonoBehaviour {
-
-    public float scale;
-    public float offset;
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class PlaceableObject : MonoBehaviour
+{
+    public float spacingOffset;
+    public float rotationOffset;
 }
