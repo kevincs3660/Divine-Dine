51d9ff16650a365df52a119da360f256d239a33c
ImpDesign <impdesign.bm@gmail.com>
2016-02-15 10:34:31 -0700

    Removed Standard Assets - Added Floor Behavior
    You can no longer place multiple objects on the same tile.

---
+++ Divine Dine/Assets/Scripts/Game Manager/PlaceObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceObject : MonoBehaviour {
+
+    public GameObject[] placeablePrefabs;     //List of objects you can place with the mouse
+
+    private bool active = false;               //If you are able to place an object with the mouse
+    private int selectedPrefab;               //Which object in the list you are going to place
+    private float prefabOffset;               //The Y offset position from the tile you are placing the new object on
+
+	void Update ()
+    {
+        if (active)
+        {
+            //Place the new object by left clicking
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if(hit.transform.tag == "Floor" && !hit.transform.GetComponent<FloorBehavior>().IsUsed())
+                    {
+                        Instantiate(placeablePrefabs[selectedPrefab],
+                            new Vector3(hit.transform.position.x, hit.transform.position.y + prefabOffset, hit.transform.position.z),
+                            hit.transform.rotation);
+
+                        hit.transform.GetComponent<FloorBehavior>().SetUsed(true);
+                        hit.transform.GetComponent<FloorBehavior>().SetObjTag(placeablePrefabs[selectedPrefab].tag);
+
+                        active = false;
+                    }
+                }
+            }
+        }
+    }
+
+    //Select a prefab from the list
+    public void SetPrefab(int prefabNumber)
+    {
+        selectedPrefab = prefabNumber;
+        prefabOffset = placeablePrefabs[selectedPrefab].GetComponent<PlaceableObject>().offset;
+    }
+
+    //Allow or disallow the placeing of objects
+    public void SetActive(bool boolean)
+    {
+        active = boolean;
+    }
+}
---
+++ Divine Dine/Assets/Scripts/Objects/FloorBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorBehavior : MonoBehaviour {
+
+    private bool used = false;
+    private string objTag = "";
+
+    public bool IsUsed()
+    {
+        return used;
+    }
+
+    public void SetUsed(bool value)
+    {
+        used = value;
+    }
+
+    public string GetObjTag()
+    {
+        return objTag;
+    }
+
+    public void SetObjTag(string value)
+    {
+        objTag = value;
+    }
+}
