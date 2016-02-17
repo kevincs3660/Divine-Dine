6b6337e4b73983246b576e65c5932b0f716986aa
Kevin Faustino <u0822559@utah.edu>
2016-02-16 17:02:44 -0700

    Cleaned up AI script

--- Divine Dine/Assets/Scripts/CustomerAI.cs
+++ Divine Dine/Assets/Scripts/CustomerAI.cs
@@ -10,24 +10,6 @@ public class CustomerAI : MonoBehaviour {
 	private NavMeshPath path;
 	void Start () {
 		path = new NavMeshPath();
-		/*
-		float distance = 0;
-		Vector3 nearestTable = new Vector3(0,0,0);
-		tables = GameObject.FindGameObjectsWithTag("Table");
-		agent = this.gameObject.GetComponent<NavMeshAgent>();
-
-		for(int i = 1; i < tables.Length; i++)
-		{
-			distance = Vector3.Distance(this.gameObject.transform.position, tables[0].gameObject.transform.position);
-			if(distance > Vector3.Distance(this.gameObject.transform.position, tables[i].gameObject.transform.position))
-			{
-				distance = Vector3.Distance(this.gameObject.transform.position, tables[i].gameObject.transform.position);
-				nearestTable = tables[i].gameObject.transform.position;
-			}
-		}
-
-		agent.SetDestination(nearestTable);*/
-	
 	}
 	
 	// Update is called once per frame
@@ -40,22 +22,19 @@ public class CustomerAI : MonoBehaviour {
 
 			agent = this.gameObject.GetComponent<NavMeshAgent>();
 
+			distance = Vector3.Distance(this.gameObject.transform.position, tables[0].gameObject.transform.position);
+			nearestTable = tables[0].gameObject.transform.position;
+
 			if(tables.Length > 1)
 			{
 				for(int i = 1; i < tables.Length; i++)
 				{
-					distance = Vector3.Distance(this.gameObject.transform.position, tables[0].gameObject.transform.position);
 					if(distance > Vector3.Distance(this.gameObject.transform.position, tables[i].gameObject.transform.position))
 					{
 						distance = Vector3.Distance(this.gameObject.transform.position, tables[i].gameObject.transform.position);
 						nearestTable = tables[i].gameObject.transform.position;
 					}
 				}
-			}
-			else 
-			{
-				distance = Vector3.Distance(this.gameObject.transform.position, tables[0].gameObject.transform.position);
-				nearestTable = tables[0].gameObject.transform.position;
 			}
 			hasMoved = true;
 			if(agent.CalculatePath(nearestTable, path))
