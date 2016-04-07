d65485cd792d603c500de6715f14fd200368dd28
ImpDesign <impdesign.bm@gmail.com>
2016-04-07 11:04:15 -0600

    Title Screen - Reimport - Clean - Full Sync

---
+++ Divine Dine/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+    public void NewGame()
+    {
+        Application.LoadLevel(1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
