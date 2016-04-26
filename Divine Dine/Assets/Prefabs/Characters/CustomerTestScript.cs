e15912883d96e5df0760fa48f1b35102ed179582
kevincs3660 <u0822559@utah.edu>
2016-04-25 19:50:16 -0600

    Test Scenes

---
+++ Divine Dine/Assets/Prefabs/Characters/CustomerTestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerTestScript : MonoBehaviour {
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = this.gameObject.GetComponent<Animator> ();
+		anim.Play ("Idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (anim.GetCurrentAnimatorStateInfo (0).IsName("Idle"));
+		Debug.Log (anim.GetCurrentAnimatorStateInfo (0).IsName("Walk"));
+	}
+}
