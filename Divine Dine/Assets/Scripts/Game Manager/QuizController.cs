624460d634963666b58e41795b39b1b24b48af37
ImpDesign <impdesign.bm@gmail.com>
2016-04-26 00:31:08 -0600

    Tutorials Continued...

---
+++ Divine Dine/Assets/Scripts/Game Manager/QuizController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuizController : MonoBehaviour
+{
+    public GameObject[] quizzes;
+    public int[] keys;
+    public GameObject rewardMenu;
+
+    private int selected;
+    private bool correct;
+
+    private GameObject panel;
+    private GameObject choice1;
+    private GameObject choice2;
+    private GameObject choice3;
+    private GameObject choice4;
+    private GameObject proceed;
+
+    void Start()
+    {
+
+    }
+
+    public void Generate()
+    {
+
+    }
+
+    public void Answer1()
+    {
+        if(correct)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Answer2()
+    {
+        if (correct)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Answer3()
+    {
+        if (correct)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Answer4()
+    {
+        if (correct)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private void CheckAnswer()
+    {
+
+    }
+}
